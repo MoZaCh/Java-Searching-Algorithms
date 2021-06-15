135a156483b94015d99e39824e8f59f7cb22cd81
Mohammed Choudhury <mzchoudhury@outlook.com>
2021-06-15 15:19:31 +0100

    Java Searching Algorithms Project

---
+++ SearchingAlgorithms.java
@@ -0,0 +1,18 @@
+package searching.algorithms;
+
+import java.util.*;
+
+/**
+ *
+ * @author mzcho
+ */
+public class SearchingAlgorithms {
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
