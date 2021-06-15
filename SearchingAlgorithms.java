9e1a002fce253bcdfdb13f1467a47f2234ae7295
Mohammed Choudhury <mzchoudhury@outlook.com>
2021-06-15 15:23:20 +0100

    Binary Search Method Implemented

--- SearchingAlgorithms.java
+++ SearchingAlgorithms.java
@@ -7,7 +7,38 @@ import java.util.*;
  * @author mzcho
  */
 public class SearchingAlgorithms {
-  
+    
+    /**
+     * 
+     * @param arr
+     * @param searchValue
+     * @return 
+     */
+    public static boolean binarySearch(int[] arr, int searchValue) {
+        
+        if(arr.length == 1) {
+            if (arr[0] == searchValue) return true;
+            
+            else return false;
+        }
+        
+        int mid = Math.round((int)arr.length/2f);
+        int[] tempArray = {};
+        
+        if(arr[mid] > searchValue) {
+            tempArray = Arrays.copyOfRange(arr, 0, mid);
+            return binarySearch(tempArray, searchValue);
+        }
+        
+        else if (arr[mid] < searchValue) {
+            tempArray = Arrays.copyOfRange(arr, mid, arr.length);
+            return binarySearch(tempArray, searchValue);
+        }
+        
+        else return true;
+    }
+      
+    
     /**
      * @param args the command line arguments
      */
