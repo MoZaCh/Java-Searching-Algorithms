dd18725274aa6f70776e67f1af10b606862d3946
Mohammed Choudhury <mzchoudhury@outlook.com>
2021-06-15 17:29:53 +0100

    Linear Search Method Implemented

--- SearchingAlgorithms.java
+++ SearchingAlgorithms.java
@@ -36,6 +36,16 @@ public class SearchingAlgorithms {
         
         else return true;
     }
+    
+    
+    public static boolean linearSearch(int[] arr, int searchValue) {
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == searchValue)
+                return true;
+        }
+        return false;
+    }
       
     
     /**
